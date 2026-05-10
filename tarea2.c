729688e9f2710271a7004f2554530f848a952972
FabianOlivares-15 <fabianolivaresp15@gmail.com>
2026-05-10 05:01:11 +0000

    Descripción clara de tus cambios

--- tarea2.c
+++ tarea2.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 typedef struct {
   char id[100];
@@ -24,10 +25,10 @@ void mostrarMenuPrincipal() {
   puts("1) Cargar Películas");
   puts("2) Buscar por id");
   puts("3) Buscar por genero");
-  puts("4) ...");
-  puts("5) ...");
-  puts("6) ...");
-  puts("7) ...");
+  puts("4) Buscar por director");
+  puts("5) Buscar por decada");
+  puts("6) Busqueda avanzada");
+  puts("7) Gestionar mi watchlist FALTA POR HACER");
   puts("8) Salir");
 }
 
@@ -112,15 +113,28 @@ void cargar_peliculas(Map *pelis_byid, Map *pelis_bygenres, Map *pelis_bydirecto
     while(director != NULL){
       MapPair *directorPair = map_search(pelis_bydirector, director);
       if(directorPair == NULL){
-        List *listaNueva = list_create();
-        list_pushBack(listaNueva, peli);
-        map_insert(pelis_bydirector, director, listaNueva);
+        List *listaNuevaDirec = list_create();
+        list_pushBack(listaNuevaDirec, peli);
+        map_insert(pelis_bydirector, director, listaNuevaDirec);
       }
       else{
         List *directorList = (List*)directorPair->value;
         list_pushBack(directorList, peli);
       }
       director = list_next(peli->director);
+    }
+    int decada = (peli->year / 10) * 10;
+    MapPair *decadaPair = map_search(pelis_bydecada, &decada);
+    if(decadaPair == NULL){
+      List *nuevaListaDeca = list_create();
+      list_pushBack(nuevaListaDeca, peli);
+      int *decadaKey = (int*)malloc(sizeof(int));
+      *decadaKey = decada;
+      map_insert(pelis_bydecada, decadaKey, nuevaListaDeca);
+    }
+    else{
+      List *decadaList = (List*)decadaPair->value;
+      list_pushBack(decadaList, peli);
     }
     
   }
@@ -137,7 +151,12 @@ void cargar_peliculas(Map *pelis_byid, Map *pelis_bygenres, Map *pelis_bydirecto
     printf("Géneros: ");
     for(char *genre = list_first(peli->genres); genre != NULL; genre = list_next(peli->genres))
       printf("%s, ", genre);
+    printf(" ");
+    printf("Director(es): ");
+    for(char *director = list_first(peli->director); director != NULL; director = list_next(peli->director))
+      printf("%s, ", director);
     printf("\n");
+    
     
     pair = map_next(pelis_byid); // Avanza al siguiente par en el mapa
   }
@@ -168,6 +187,123 @@ void buscar_por_id(Map *pelis_byid) {
     printf("La película con id %s no existe\n", id);
   }
 }
+void buscar_por_genre(Map *pelis_bygenres){
+  char genre[25];
+  printf("Ingrese el genero de la pelicula: ");
+  scanf(" %[^\n]", genre);
+  MapPair *pair = map_search(pelis_bygenres, genre);
+  if(pair != NULL){
+    List *peliculasDeGenre = (List*)pair->value;
+    Film *peli = (Film*)list_first(peliculasDeGenre);
+    printf("\n Peliculas del genero %s \n", genre);
+    while(peli != NULL){
+      printf(" Titulo: %s | Año: (%d) | Rating: %1.f | Director(es): ", peli->title, peli->year, peli->rating);
+      char *dir = (char*)list_first(peli->director);
+      while(dir != NULL){
+        printf("%s", dir);
+        dir = (char*)list_next(peli->director);
+        if(dir != NULL){
+          printf(", ");
+        }
+      printf("\n");
+      peli = (Film*)list_next(peliculasDeGenre);
+      }
+    }
+  }
+  else{
+    printf("No se encontraron peliculas del genero %s", genre);
+  }
+}
+
+void buscar_por_director(Map *pelis_bydirector){
+  char director[25];
+  printf("Ingrese el director de la pelicula: ");
+  scanf(" %[^\n]", director);
+  MapPair *pair = map_search(pelis_bydirector, director);
+  if(pair != NULL){
+    List *peliculasDeDirector = (List*)pair->value;
+    Film *peli = (Film*)list_first(peliculasDeDirector);
+    printf("\nPeliculas del director %s \n", director);
+    while(peli != NULL){
+      printf("Titulo: %s | Año: (%d) | Rating: %1.f | Genero(s): ", peli->title, peli->year, peli->rating);
+      char *genre = (char*)list_first(peli->genres);
+      while(genre != NULL){
+        printf("%s", genre);
+        genre = (char*)list_next(peli->genres);
+        if(genre != NULL){
+          printf(", ");
+        }
+      printf("\n");
+      peli = (Film*)list_next(peliculasDeDirector);
+      }
+    }
+  }
+  else{
+    printf("No se encontraron peliculas del director %s", director);
+  }
+}
+
+void buscar_por_decada(Map *pelis_bydecada){
+  int anioPeliculas;
+  printf("Ingrese el año para buscar su decada: ");
+  scanf("%d", &anioPeliculas);
+
+  int buscarDecada = (anioPeliculas / 10) * 10;
+  MapPair *pair = map_search(pelis_bydecada, &buscarDecada);
+  if(pair != NULL){
+    List *peliculasPorDecada = (List*)pair->value;
+    Film *peli = (Film*)list_first(peliculasPorDecada);
+    printf("Peliculas de la decada de los %d\n", buscarDecada);
+    while(peli != NULL){
+      printf("Titulo: %s | Año: (%d) | Rating: %1.f | Director(es): ", peli->title, peli->year, peli->rating);
+      char *director = (char*)list_first(peli->director);
+      while(director != NULL){
+        printf("%s", director);
+        director = (char*)list_next(peli->director);
+        if(director != NULL){
+          printf(", ");
+        }
+      }
+      printf("\n");
+      peli = (Film*)list_next(peliculasPorDecada);
+    }
+  }
+  else{
+    printf("No se encontraron peliculas de esta decada %d\n", buscarDecada);
+  }
+}
+
+void busqueda_avanzada(Map *pelis_bygenres){
+  char genre[25];
+  int anioBuscar;
+  printf("Ingrese el genero: ");
+  scanf(" %[^\n]", genre);
+  
+  printf("Ingrese el año para buscar la decada: ");
+  scanf("%d", &anioBuscar);
+  int decadaBuscada = (anioBuscar / 10) * 10;
+  MapPair *pair = map_search(pelis_bygenres, genre);
+  if(pair != NULL){
+    List *listaPeliculas = (List*)pair->value;
+    Film *peli = (Film*)list_first(listaPeliculas);
+    bool encontradas = false;
+    printf("Peliculas del genero %s en la decada de los %d\n", genre, decadaBuscada);
+    while(peli != NULL){
+      int decadaPeli = (peli->year / 10) * 10;
+      if(decadaPeli == decadaBuscada){
+        printf("Titulo: %s | Año: (%d) | Rating: %1.f\n", peli->title, peli->year, peli->rating);
+        encontradas = true;
+      }
+      peli = (Film*)list_next(listaPeliculas);
+    }
+    if(!encontradas){
+      printf("No hay peliculas del genero %s en la decada de los %d\n", genre, decadaBuscada);
+    }
+  }
+  else{
+    printf("No existe el genero %s", genre);
+  }
+}
 
 int main() {
   char opcion; // Variable para almacenar una opción ingresada por el usuario
@@ -179,6 +315,7 @@ int main() {
   Map *pelis_bygenres = map_create(is_equal_str);
   Map *pelis_bydirector = map_create(is_equal_str);
   Map *pelis_bydecada = map_create(is_equal_int);
+  List *miWatchlist = list_create();
 
   // Recuerda usar un mapa por criterio de búsqueda
 
@@ -198,10 +335,13 @@ int main() {
       buscar_por_genre(pelis_bygenres);
       break;
     case '4':
+      buscar_por_director(pelis_bydirector);
       break;
     case '5':
+      buscar_por_decada(pelis_bydecada);
       break;
     case '6':
+      busqueda_avanzada(pelis_bygenres);
       break;
     case '7':
       break;
